ab43ff7fadc61eb9e2ce40327ea071ba5e1b8906
Jordan Crow <jordanbcrow@gmail.com>
2025-07-18 16:23:52 +0200

    Implement translations functionality including getting browser language, fix semantic bug in html

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Replaces each element in html with translation in current language
 function switchLanguage(lang) {
     const elements = document.querySelectorAll('[data-i18n]');
     elements.forEach(el => {
@@ -6,14 +7,20 @@ function switchLanguage(lang) {
     });
 }
 
-const defaultLang = "en";
-setLanguage(defaultLang);
-
 document.addEventListener("DOMContentLoaded", () => {
-    setLanguage(defaultLang);
+    // Get user's browser language
+    const userLang = navigator.language.slice(0, 2);
+    const supportedLanguages = ["en", "es", "ca"];
+    // If user language not supported, set default language to English
+    const defaultLang = supportedLanguages.includes(userLang) ? userLang : "en";
+    
+    // Set Language
+    switchLanguage(defaultLang);
 });
 
-document.querySelectorAll('language-switcher li').forEach(li => {
+
+// Switch language when user clicks on language flag
+document.querySelectorAll('.language-switcher li').forEach(li => {
     li.addEventListener('click', () => {
         const lang = li.getAttribute('data-lang');
         switchLanguage(lang);
